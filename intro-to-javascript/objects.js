8a1fe52f69b8d6f6585b8f4170ff6743d67fe2fc
uly <michoel966@gmail.com>
2024-06-03 23:15:25 -0500

    add all folders

---
+++ intro-to-javascript/objects.js
@@ -0,0 +1,54 @@
+let person = {
+    name: "Baruch",
+    age: 25,
+    occupation: "Shaliach"
+};
+console.log(person["name"]);
+console.log(person.name);
+console.log(person["age"]);
+console.log(person.age);
+let car = {
+    make: "toyota",
+    model: "camry",
+    start: function () {
+        console.log("Engine started!");
+    }
+};
+console.log(car.make, car.model)
+car.start();
+let cars = [
+{
+    make:"Toyota",
+    model: "Corolla",
+    year: 2022,
+    color: "Silver",
+    features: ["Automatic Transmission", "Touchscreen Infotainment", "Lane Departure Warning"],
+},
+{
+    make: "Ford",
+    model: "Mustang",
+    year: 2022,
+    color: "Red",
+    features: ["Abc"],
+},
+{
+    make: "Honda",
+    model: "Civic",
+    year: 2022,
+    color: "Blue",
+    features:["Abc"]
+}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
